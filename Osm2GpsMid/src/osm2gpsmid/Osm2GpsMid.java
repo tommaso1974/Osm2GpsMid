f286819766f9c6350eda00f5ea13ab10c75e7bbb
tommaso grenga <>
2013-10-22 17:42:59 +0200

    aggiunte le librerie e creato il progetto per netbeans

---
+++ Osm2GpsMid/src/osm2gpsmid/Osm2GpsMid.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package osm2gpsmid;
+
+/**
+ *
+ * @author nexse
+ */
+public class Osm2GpsMid {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
