21a8db0fd8f471bc637596f747f4b3bbf722311e
tommaso grenga <>
2013-10-22 17:52:14 +0200

    Aggiunta tutta l'infrastruttura delle cartelle......da eliminare quello che non serve e da modificare il modo con cui andare a parsare il file di properties

---
+++ Osm2GpsMid/src/de/ueller/osmToGpsMid/model/ConditionTuple.java
@@ -0,0 +1,15 @@
+package de.ueller.osmToGpsMid.model;
+
+
+public class ConditionTuple {
+	public String key;
+	public String value;
+	public boolean exclude;
+	public boolean regexp;
+	public boolean properties;
+	
+	public String toString() {
+		return "Specialisation " + key + "=" + value + " excluded:" + exclude +
+		    " regexp: " + regexp + " properties: "+ properties;
+	}
+}
