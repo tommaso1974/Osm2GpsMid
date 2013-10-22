21da307bf49497a446e9c7f71c613e6bfc14f9d2
tommaso grenga <>
2013-10-22 19:24:50 +0200

    struttura di base del file di properties

--- Osm2GpsMid/src/osm2gpsmid/PropertiesCommonUtils.java
+++ Osm2GpsMid/src/osm2gpsmid/PropertiesCommonUtils.java
@@ -6,6 +6,37 @@ package osm2gpsmid;
 public class PropertiesCommonUtils {
     
     /*
+    midlet.name = GPSmid-MyTown
+    map.name = GPSmid-MyTown
+    mapSource = /path/to/file.osm.pbf
+    * 
+    the upper value to "region.1.lat.min",
+    the lower value to "region.1.lat.max",
+    the left value to "region.1.lon.min" and
+    the right value to "region.1.lon.max". 
+
+*  region.1.lat.min = -90
+ region.1.lat.max = 90
+ region.1.lon.min = -180
+ region.1.lon.max = 180
+
+ region.2.lat.min = 40.00
+ region.2.lat.max = 11.36
+ region.2.lon.min = 40.94
+ region.2.lon.max = 12.05
+ * 
+style-file = style-file.xml
+* app = GpsMid-Generic-full
+* keepTemporaryFiles = true
+* useRouting = motorcar,bicycle,foot
+* http://sourceforge.net/apps/mediawiki/gpsmid/index.php?title=Properties_file
+    * 
+    * 
+    * 
+    * /
+    
+    
+    /*
      Il file di properties deve contenere, come minimo
      * Il percorso dove andare a prendere il file compresso/osm
      * Se andare a processare i dati e travasarli sul database
